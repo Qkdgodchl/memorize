3138639ef306d7b0c710ac404aa5a0b36d76c071
이영민 <fkvkxk1@naver.com>
2024-07-01 13:21:31 +0900

    글자 색 변경

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("gogogo")
                 .font(.largeTitle)
+                .foregroundStyle(.red)
         }
         .padding()
     }
