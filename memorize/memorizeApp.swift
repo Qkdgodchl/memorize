34e812dbc3ed22374b7b5ff399dba007ed7a6793
이영민 <fkvkxk1@naver.com>
2024-07-01 13:16:05 +0900

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by 이영민 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
